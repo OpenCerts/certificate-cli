03ffa2c8a37bc5ef8123e953e95f77cc82c4c6e7
Unknown <me@rjchow.com>
2019-03-11 14:32:48 +0800

    benchmarks

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,1 +1,9 @@
-# TODO
+#!/bin/bash
+
+time node index.js batch ./benchmark/unsigned_certs ./benchmark/signed_certs
+rm -rf ./benchmark/signed_certs
+time node index.js batch ./benchmark/unsigned_certs ./benchmark/signed_certs
+rm -rf ./benchmark/signed_certs
+time node index.js batch ./benchmark/unsigned_certs ./benchmark/signed_certs
+rm -rf ./benchmark
+
