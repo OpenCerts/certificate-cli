24137230c43bda530d9b5a5689324093c5acf12e
Raymond Yeh <yehjxraymond@gmail.com>
2019-12-05 10:23:35 +0800

    chore: elaborate error on read fail

--- src/diskUtils.js
+++ src/diskUtils.js
@@ -2,13 +2,19 @@ const fs = require("fs");
 const util = require("util");
 const path = require("path");
 const { filter, some } = require("lodash");
+const { logger } = require("../lib/logger");
 
 const readdir = util.promisify(fs.readdir);
 
 const opencertsFileExtensions = [/(.*)(\.)(opencert)$/, /(.*)(\.)(json)$/];
 
 function readCert(directory, filename) {
-  return JSON.parse(fs.readFileSync(path.join(directory, filename)));
+  try {
+    return JSON.parse(fs.readFileSync(path.join(directory, filename)));
+  } catch (e) {
+    logger.error(`Failed at ${directory}${filename}`);
+    throw e;
+  }
 }
 
 function isOpenCertFileExtension(filename) {
